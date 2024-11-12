33fcdbe1a954f0ea553e328bb436712129c6b30e
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-11-12 10:29:06 +0530

    added new port 9999

--- runserver.ps1
+++ runserver.ps1
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 cd restserver
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8888
+python manage.py runserver 0.0.0.0:9999
