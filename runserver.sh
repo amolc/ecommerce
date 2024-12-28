1df9c1d1b9b12e04b75b739559c16c29fdf4d069
Ubuntu <ubuntu@ip-172-31-40-146.ap-southeast-1.compute.internal>
2024-12-28 07:27:06 +0000

    Fixed merge conflicts.

--- runserver.sh
+++ runserver.sh
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-# /bin/bash
-=======
 #!/bin/bash
 # ps -ef | grep "manage.py runserver" | awk '{print $2}' | xargs kill -9 
->>>>>>> 5ba058317c8dd5601655eb264346067760b275b0
+
 source scripts/configs/stayvillas.sh
 source venv/bin/activate
 pip install -r requirements.txt
 cp -R docs/build/html restserver/static/
 cd restserver
 python manage.py migrate
-<<<<<<< HEAD
+
 python manage.py runserver 0.0.0.0:9999
 
 
-=======
-python manage.py runserver 0.0.0.0:9999
->>>>>>> 5ba058317c8dd5601655eb264346067760b275b0
