4a8fc7c5ab5497084d19d2c25f4eb4f938c7aa7f
Amol C <amolch001@gmail.com>
2025-01-15 16:39:57 +0530

    added only the ecommerce scripts config

--- scripts/configs/quantbots.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exporting env variables to be used with django server
-
-# frontend config
-FRONTEND_DEST="/home/ubuntu/quantbots"
-FRONTEND_REPO_DIR="/home/ubuntu/repos/quantbots"
-FRONTEND_REPO_BRANCH="main"
-FRONTEND_EXCLUDE_DIRS="/.git/ /node_modules/"
-FRONTENDSUPERVISORNAME="quantbots"
