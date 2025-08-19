0caa48e7b0579fc25abe6eeb64a302f129221d2f
Amol C <amolch001@gmail.com>
2025-08-19 17:15:48 +0530

    added the ps1

--- runserver.ps1
+++ runserver.ps1
@@ -4,6 +4,8 @@
 pip install -r requirements.txt
 Set-Location restserver
 
+$env:DJANGO_SETTINGS_MODULE = 'restserver.settings.development'
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:9999
